18f1856558484428bf0400f402cc12b73bdc94ca
schreinibert <schreinibert@live.de>
2013-12-09 19:01:16 +0100

    Rahmen für Datenstruktur angelegt

---
+++ js/myAtom.js
@@ -0,0 +1,58 @@
+
+var element = atom();
+
+element.set({
+	level: {
+		icon: ,
+		windows: ,
+		roof: true,
+		roofAttributes: {
+		},
+		floor: true,
+		floorAttributes: {
+			doors:
+		},
+		sgml: true,
+		smglAttributes: {
+			editorContent: ,
+		},
+		xml: true,
+		xmlAttributes: {
+			editorContent: ,
+		},
+		html: true,
+		htmlAttributes: {
+			editorContent: ,
+		},
+	},
+	window: {
+		icon: ,
+		sgml: true,
+		smglAttributes: {
+			editorContent: ,
+		},
+		xml: true,
+		xmlAttributes: {
+			editorContent: ,
+		},
+		html: true,
+		htmlAttributes: {
+			editorContent: ,
+		},
+	},
+	door: {
+		icon: ,
+		sgml: true,
+		smglAttributes: {
+			editorContent: ,
+		},
+		xml: true,
+		xmlAttributes: {
+			editorContent: ,
+		},
+		html: true,
+		htmlAttributes: {
+			editorContent: ,
+		},
+	},
+});
