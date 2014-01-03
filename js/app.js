98974776d86bddcd7f21e8ae72bbfe7e02d1444d
Simon Jentsch <sjentsch@me.com>
2014-01-03 21:26:24 +0100

    Diff mapper + remove diff notes +  close brackets für Codemirror

--- js/app.js
+++ js/app.js
@@ -1,3 +1,83 @@
+// Thanks to Stack Overflow user "sbgoran" for the basis of this deepDiffMapper
+var deepDiffMapper = function() {
+    return {
+        VALUE_CREATED: 'created',
+        VALUE_UPDATED: 'updated',
+        VALUE_DELETED: 'deleted',
+        VALUE_UNCHANGED: 'unchanged',
+        map: function(obj1, obj2) {
+            if (this.isFunction(obj1) || this.isFunction(obj2)) {
+                throw 'Invalid argument. Function given, object expected.';
+            }
+            if (this.isValue(obj1) || this.isValue(obj2)) {
+                var output = obj1 || obj2;
+                output.diffState = this.compareValues(obj1, obj2);
+                return output;
+            }
+            
+            if (this.isArray(obj1) && this.isArray(obj2)) {
+                var diff = [];
+            } else {
+                var diff = {};
+            }
+            
+            for (var key in obj1) {
+                if (this.isFunction(obj1[key])) {
+                    continue;
+                }
+                
+                if (key == 'unit')
+                    diff[key] = obj1[key];
+
+                var value2 = undefined;
+                if ('undefined' != typeof(obj2[key])) {
+                    value2 = obj2[key];
+                }
+                
+                diff[key] = this.map(obj1[key], value2);
+            }
+            for (var key in obj2) {
+                if (key == 'unit')
+                    diff[key] = obj2[key];
+                
+                if (this.isFunction(obj2[key]) || key == 'unit' || ('undefined' != typeof(diff[key]))) {
+                    continue;
+                }
+
+                diff[key] = this.map(undefined, obj2[key]);
+            }
+
+            return diff;
+
+        },
+        compareValues: function(value1, value2) {
+            if (value1 === value2) {
+                return this.VALUE_UNCHANGED;
+            }
+            if ('undefined' == typeof(value1)) {
+                return this.VALUE_CREATED;
+            }
+            if ('undefined' == typeof(value2)) {
+                return this.VALUE_DELETED;
+            }
+
+            return this.VALUE_UPDATED;
+        },
+        isFunction: function(obj) {
+            return toString.apply(obj) === '[object Function]';
+        },
+        isArray: function(obj) {
+            return toString.apply(obj) === '[object Array]';
+        },
+        isObject: function(obj) {
+            return toString.apply(obj) === '[object Object]';
+        },
+        isValue: function(obj) {
+            return !this.isObject(obj) && !this.isArray(obj);
+        }
+    }
+}();
+
 (function ( $ ) {
     
     // HTML Baustelle Main Object
@@ -33,6 +113,7 @@
         var clouds = [];
         
         var houseStruct = [];
+        var diffStruct = {};
         
         var updatedLines = [1,3];
 
@@ -64,6 +145,7 @@
               lineNumbers: true,
               indentUnit: indent,
               autoCloseTags: autoClose,
+              autoCloseBrackets: true,
               theme: "lesser-dark"
             });
                         
@@ -206,6 +288,24 @@
             }
             window.setTimeout(function () {removeEditorHighlights() }, 3000);
         }
+        
+        /// Löscht die diffState Einträge in der Datenstruktur
+        function removeDiffNotes (struct) {
+            for (var i = struct.length-1; i>=0; i--) {
+                if (typeof struct[i].diffState !== 'undefined') {
+                    // Wenn Diff State "deleted" ist, dann lösche den kompletten Node
+                    if (struct[i]['diffState'] == 'deleted') {
+                        struct.splice(i--, 1);
+                        continue;
+                    }
+                    // ansonsten lösche einfach den diffState Eintrag
+                    delete struct[i]['diffState'];
+                }
+                if (typeof struct[i].childNodes !== 'undefined' && struct[i].childNodes.length > 0) {
+                    removeDiffNotes(struct[i].childNodes);
+                }
+            }
+        }
     
         /// Funktion, die aufgerufen wird, wenn neuer Inhalt in Code Editor eingegeben wird
         this.checkEditor = function () {
@@ -222,8 +322,25 @@
                 // Wandelt String in DOM um
                 var editorDOM = $(editorValue);
                 // Convertiert DOM in Haus-Struktur und speichert diese
-                houseStruct = convertDomToStruct(editorDOM, 'root');
+                var newStruct = convertDomToStruct(editorDOM, 'root');
                 
+                houseStruct = deepDiffMapper.map(houseStruct, newStruct);
+                /*console.log(newStruct);
+                console.log(diffStruct);
+                */
+                removeDiffNotes(houseStruct); // rauslöschen! Muss in die Render Update Methode benutzen, nachdem neu gezeichnet wurde
+                
+                /* TO KILL START */
+                var editorText = "";
+                $.each(houseStruct, function(index, value) {
+                   var textToAdd = addTagToEditor(value, 0);
+                   editorText += textToAdd;
+                   
+                });
+                editorText = editorText.substr(0, editorText.length-1);
+                $('#debug-output').html(editorText);
+                /* TO KILL END */
+            
             } catch (e) {
                 console.log(e.name +"==> "+ e.message);
             }
